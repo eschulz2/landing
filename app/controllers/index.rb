535e53aac6022a2c8cb035168e4bfdb254b3c675
Eric Schulz <eschulz2@gmail.com>
2014-01-27 17:09:43 -0600

    models, migrations, and basic crud are functioning

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,19 @@
-get '/' do
-  # Look in app/views/index.erb
+get '/dumpweed' do
+  @contacts = Contact.order('first_name')
   erb :index
+end
+
+get '/' do
+  @contact = Contact.new(params[:contact])
+  erb :new
+end
+
+post '/contacts' do 
+  puts params.inspect
+  @contact = Contact.new(params[:contact])
+  if @contact.save
+  	redirect to('/')
+  else
+  	erb :new
+  end
 end
